c2ddaa5496a1d7f5e06fa793386020bf09bb33ac
alexisflip <henao.aranzazu@gmail.com>
2025-08-03 16:00:46 +0200

    PlayerName: Save Edit working
    Made a new useState to handle the name input. Use the controlled input called two-way binding for inputing and outputing text in a field at the same time.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,31 @@
 import { useState } from "react"
 
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol}){
 
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
     function handleSelect (){
-        setIsEditing(true);
+        setIsEditing(editing => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>
+    function handleChange (event){
+        setPlayerName(event.target.value);
+    }
+
+    let inPlayerName = <span className="player-name">{playerName}</span>
     
     if (isEditing) {
-        playerName = <input type="text" required/> 
+        inPlayerName = <input type="text" required value={playerName} onChange={handleChange}/> 
     }
 
     return(
         <li>
             <span className="player">
-                {playerName}
+                {inPlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleSelect}>eii</button>
+            <button onClick={handleSelect}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     )
 }
